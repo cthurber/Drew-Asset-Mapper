ba3260d198e9ce8ce9e8c8287e8df11fe67317d1
Ryan Otto <rotto@drew.edu>
2016-10-18 15:45:57 -0400

    Updated map tile layer/updated Zoom level
    Tile layer/zoom level is now more user friendly,
    
    ~Controller Team

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,11 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-var mymap = L.map('mapid').setView([51.505, -0.09], 18);
+var mymap = L.map('mapid').setView([40.76804,-74.235692], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18, minZoom:3,
-    id: 'mapbox.satellite',
+    id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoicnlhbm90dG8xIiwiYSI6ImNpdWVyNHNtNjAwYXUydHBncTNvcjQ4azgifQ.y_p9wdDpPsdOqzUEAQBNlg'
 }).addTo(mymap);
     mymap.invalidateSize()
