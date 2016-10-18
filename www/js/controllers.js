769799aed1c59af7b04faa6bdcac8530366d12f3
Ryan Otto <rotto@drew.edu>
2016-10-18 16:05:54 -0400

    Updated map height
    Changed map height to 600 to fill more of the window
    
    ~Controller Team

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoicnlhbm90dG8xIiwiYSI6ImNpdWVyNHNtNjAwYXUydHBncTNvcjQ4azgifQ.y_p9wdDpPsdOqzUEAQBNlg'
 }).addTo(mymap);
-    mymap.invalidateSize()
+    mymap.invalidateSize();
 
 }])
    
