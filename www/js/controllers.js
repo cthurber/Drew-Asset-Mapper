147574986234164ebdb65910b9871afe3a5c687c
Ryan Otto <rotto@drew.edu>
2016-10-25 15:19:20 -0400

    Disabled Side Menu Scroll on Map View
    When side scrolling, side menu no longer opens up.  User must click side
    menu button to open it.
    
    ~Jon/Ryan

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,16 @@
 angular.module('app.controllers', [])
-  
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+.controller('homeCtrl', ['$scope', '$stateParams', '$ionicSideMenuDelegate',
+function ($scope, $stateParams, $ionicSideMenuDelegate) {
+
+  $scope.$on('$ionicView.enter', function(){
+      $ionicSideMenuDelegate.canDragContent(false); //Disable side scrolling opening up side menu (Jon, Ryan)
+    });
+
+
 var mymap = L.map('mapid').setView([40.76804,-74.235692], 13); //Set map name and default view location/zoom level
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //Initialize map with mapbox streets tileset and min/max zoom level
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -12,13 +19,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoicnlhbm90dG8xIiwiYSI6ImNpdWVyNHNtNjAwYXUydHBncTNvcjQ4azgifQ.y_p9wdDpPsdOqzUEAQBNlg'}).addTo(mymap);
     mymap.invalidateSize(); //Fixes grey tile rendering issue
 
+     //Will become a loop pulling coordinates from the table, markers receive LatLng objects
      var markerIcon = L.icon({iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-orange.png', iconSize:[38, 60]}); //Create a custom marker icon
      var marker = L.marker([40.76, -74.235], {icon: markerIcon}).addTo(mymap).bindPopup("I am an orange leaf."); //Add marker with custom icon to map
      var marker2 = L.marker([40.75, -74.235], {icon: markerIcon}).addTo(mymap).bindPopup("I am a slightly more informative orange leaf."); //Add marker with custom icon to map
      var marker3 = L.marker([40.74, -74.240], {icon: markerIcon}).addTo(mymap).bindPopup("I am an equally orange leaf."); //Add marker with custom icon to map
      var marker4 = L.marker([40.78, -74.245], {icon: markerIcon}).addTo(mymap).bindPopup("I am an orange leaf without a sense of humor."); //Add marker with custom icon to map
-
-
 
 
 
