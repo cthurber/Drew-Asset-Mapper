3d1f4fe2c7c12db3a0a69f42495ef36ff974cfd0
Chris Thurber <cthurber@me.com>
2016-10-26 14:01:09 -0400

    Removed attribution clutter from map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,7 @@ function ($scope, $stateParams, $ionicSideMenuDelegate) {
 
 var mymap = L.map('mapid').setView([40.76804,-74.235692], 13); //Set map name and default view location/zoom level
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //Initialize map with mapbox streets tileset and min/max zoom level
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: '',
     maxZoom: 18, minZoom:3,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoicnlhbm90dG8xIiwiYSI6ImNpdWVyNHNtNjAwYXUydHBncTNvcjQ4azgifQ.y_p9wdDpPsdOqzUEAQBNlg'}).addTo(mymap);
@@ -47,7 +47,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 
 }])
-   
+
 .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -55,7 +55,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -63,4 +63,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
